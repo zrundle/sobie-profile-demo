148949be3ebfae51eedaaa1e1aa76707c8496677
Rundle <zrundle@una.edu>
2025-01-23 16:42:25 -0600

    COMMMIT EJS CHANGES AND POSTGET VARIABLES

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
+//node app.js to run code 
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'))
 
 
 app.get('/', function (req, res) {
     res.sendFile('index.html');
+})
+
+app.post('/saveName', (req, res)=>{
+    console.log('hit POST endpoint?');
+
+    console.log(req.body);
+    res.redirect('/ejs');    
+})
+
+app.get('/saveNameGet', (req, res)=>{
+    console.log('hit GET endpoint?');
+
+    console.log(req.query);
+    res.render('words',
+        {pageTitle: req.body.myName}
+    );
+    // res.redirect('/ejs');
+    
 })
 
 // You need to type "/ejs" after localhost:3000 to find the ejs render webpage
